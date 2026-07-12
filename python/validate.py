7efcbaf3ff9261704e8e40463d9ba5a397997adc
Pallavikujur <pallavikujur98@gmail.com>
2026-07-12 18:53:46 +0530

    Update validate.py

--- python/validate.py
+++ python/validate.py
@@ -1,11 +1,13 @@
 print("Monitoring Validation Started")
 
 import json
-with open("../payloads/request.json" as file, "r"):
+with open("../payloads/request.json","r")as file:
   content = json.load(file)
   print("Hostname:", content["hostname"])
   if (content["hostname"] == ""):
       print("validation failed")
+  else:
+      print("validation successfull")
     
     
   
